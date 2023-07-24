9b7b2c8c05c328dff439e60c77479a5e26e1292f
daeunparkpark <14512024@naver.com>
2023-07-24 16:52:57 +0900

    first commit

---
+++ # DemoIndexing.py
@@ -0,0 +1,23 @@
+# DemoIndexing.py
+strA = "python is very powerful"
+print( strA[0])
+print( strA[1])
+print( strA[0:3])
+#축약된 (약식) 표현식
+print( strA[:3])
+#디버깅하지않고 바로실행 단축기 ctrl + f5
+print(strA[-3:])
+print(strA[-8:])
+
+#리스트 연습
+colors = ["red","blue", "green"]
+print(colors)
+print(len(colors))
+print(colors[0])
+
+
+#디버깅할때 중단점 (break point)
+for item in colors:
+    print(item)
+
+colors.append
---
+++ demo.py
@@ -0,0 +1,9 @@
+# demo.py
+
+print("hello vs code")
+
+strA = "python"
+strB = "파이썬은 강력해"
+x=100
+y= 3.14
+dir()
